de8491e467e30326215207b79af80becade0e129
Saïd Redouane <s_red009@hotmail.com>
2021-02-09 20:07:02 +0000

    fixed my comments

--- spinner1.js
+++ spinner1.js
@@ -18,17 +18,14 @@ setTimeout(() => {
 }, 700);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r|   ");
 }, 900);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r/   ");
 }, 1100);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r-   ");
 }, 1300);
 
@@ -38,11 +35,9 @@ setTimeout(() => {
 }, 1500);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r|   ");
 }, 1700);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\n");
 }, 1700);
